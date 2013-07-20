529ad072874f15eec42bd8a1e1ad201937681e98
rajulonline <rajulonline@gmail.com>
2013-07-19 23:27:09 -0700

    Method name change
    Changed method name

--- app/controllers/tools_controller.rb
+++ app/controllers/tools_controller.rb
@@ -1,6 +1,5 @@
 class ToolsController < ApplicationController
   def index
     reset_session
-
   end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,4 +36,6 @@ Learning::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+  
+  config.assets.enabled = true
 end
