eb2993acb4abf1fe3f816ce36dff3f104668265b
rajulonline <rajulonline@gmail.com>
2013-07-23 12:28:16 -0700

    Revert changes to app controller
    revert appl controller flash changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,18 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  
-  after_filter :flash_to_headers
-
-def flash_to_headers
-  return unless request.xhr?
-  response.headers['X-Message'] = flash[:notice]  unless flash[:notice].blank?
-  # repeat for other flash types...
-
-  flash.discard  # don't want the flash to appear when you reload page
-end
-  
-  
-  
   def cache_everything
     @post = Post.all_cached
     @children = Children.all_cached
