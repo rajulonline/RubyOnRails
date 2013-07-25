5ea8ad6af8f6e62c8dacac99112fedc8a0d52fa2
rajulonline <rajulonline@gmail.com>
2013-07-25 15:33:56 -0700

    Edit child test cases
    This is for editing child test cases

--- app/controllers/children_controller.rb
+++ app/controllers/children_controller.rb
@@ -1,21 +1,17 @@
 class ChildrenController < ApplicationController
-
   def add_child_test_case
 
     @post = Post.find(params[:post])
 
   end
 
-
   def display_child_test_case
-
 
     # logger.error("__________#{request.post?}------------")
 
     post = params[:id]
     @post = Post.find((post))
 
-   
     @children = Children.create(params[:children])
     @children.parent_tc_id  = @post.parent_tc_id
     @children.agent = @post.agent
@@ -40,9 +36,39 @@ class ChildrenController < ApplicationController
   def display_all_child_test_cases
     @children = Children.find(:all)
     if @children.nil?
-    flash[:notice]='There are no child test cases'
+      flash[:notice]='There are no child test cases'
     end
-      
+
   end
- 
+
+  def edit_child_test_case
+
+    if request.post?
+      
+      @post = Post.find(:all)
+      @login = Login.select('DISTINCT user')
+      @status = Status.find(:all)
+      @post_param = Post.find_by_parent_tc_id(params[:parent_tc_id])
+      @children = @post_param.childrens.find(params[:id])
+
+    end
+    @children.save
+    respond_to do |format|
+      format.html # index.html.erb
+      format.js
+    #render :update do |page| page.replace_html "div_id", :partial => "new_content"
+    end
+  end
+
+  def update_child_test_case
+    children = Children.find(params[:children]['id'])
+    children.update_attributes(params[:children])
+    #get all records
+    @children = Children.all
+    respond_to do |format|
+      format.js
+    end
+  end
+
 end
+
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+def modify_truncate(content,length)
+ truncate(content, :length => length)
+end
 end
