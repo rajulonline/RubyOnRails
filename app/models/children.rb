bd13190c17af8e065b1f7cdad7a4e8bb21e1f44e
rajulonline <rajulonline@gmail.com>
2013-07-30 12:47:48 -0700

    cosmetic changes to child test cases display
    cosmetic changes to child test cases display

--- app/models/children.rb
+++ app/models/children.rb
@@ -1,8 +1,9 @@
 class Children < ActiveRecord::Base
-  attr_accessible :child_tc_id, :content, :status, :agent, :parent_tc_id
+  attr_accessible :child_tc_id, :content, :status, :agent, :parent_tc_id,:title
   belongs_to :post, :foreign_key=>:parent_tc_id
   belongs_to :requirement,:foreign_key=>:req_id
   
+  validates :title, :presence => true
   validates :content, :presence => true
   validates :status, :presence => true
  
